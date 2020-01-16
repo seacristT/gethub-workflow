f1c7dcf414e133ff078cf836f310efc985d5ffe7
tyler <seacristta@g.cofc.edu>
2020-01-16 17:32:41 -0500

    Hello World Program github workflow homework

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public class Main {
+        public void main(String[] args) {
+            System.out.println("Hello World!");
+        }
+    }
+}
